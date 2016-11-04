5ea4072a9835984b483822ce0c4f3bfe2096e44c
Narendra <Narendra@client2>
2016-11-04 10:09:39 +0530

    Initial project has been setup

---
+++ SpringHibernateDemo/src/com/LoginAction.java
@@ -0,0 +1,62 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginAction
+ */
+@WebServlet("/LoginAction")
+public class LoginAction extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginAction() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Http     Servlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		
+		
+
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		
+		String name = null;
+		String password=null;
+		String nam=request.getParameter(name);
+		String pass=request.getParameter(password);
+		
+		response.setContentType("text/html");
+		
+		PrintWriter out=response.getWriter();
+		out.println("User Name is :"+nam);
+		out.println("User password is :"+pass);
+		
+		doGet(request, response);
+		
+	}
+
+}
